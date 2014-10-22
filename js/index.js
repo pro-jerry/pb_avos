ee61c6a532a1b867def9321b5ff03d221aaf9f8f
weidays <weidays@qq.com>
2014-10-22 12:32:11 +0800

    修改变化

--- js/index.js
+++ js/index.js
@@ -1,28 +1,25 @@
 $(document).on("pageinit", "#index", function() {
 	// 绑定提交按钮事件
 	$(document).on("click", "#toLogin", function(event) {
-		var url='http://'+window.location.host+'/parentsbank/bankowner/json_bankOwnerLogin.aspx';
-		//console.log(url);
-		var param = {
+	/*	var param = {
 			"bankOwner.userName":$("#userName").val(),
 			"bankOwner.password":$("#password").val()
-		};
-		$.ajax({
-			type : 'POST',
-			url : url,
-			data: param,
-			dataType : 'json',
-			success : function(data) {
-				if(data.state.code==0){
-					window.location.href="main.jsp";
-				}else{
-					alert(data.state.msg);
-					console.log(data.state.debugMsg);
-				}
-			},
-			error : function(data) {
-				console.log(data.state.debugMsg);
-			}
-		});
+		};*/
+
+        AV.initialize("nt9nujwnc0vp7g1htejarpovprpsy6inrrr2ixeobaozy0xe", "k9beb8y6876zbhp5cgk4p83swuri1y2gc10f8y1tml07haam");
+        var Test = AV.Object.extend('test');
+        var query = new AV.Query(Test);
+        query.find({
+            success: function(results) {
+                console.log(results);
+                if(results && results.length>0){
+                    document.getElementById('target').innerHTML = "id:" +results[0].id +"<br/>createAt:" + results[0].createdAt + "<br/>updateAt:" + results[0].updatedAt;
+
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
 	});
 });
