a11936576d0c8e4bf77d4ddbc0bf7afa851bc5c1
vessnix <vessnix@gmail.com>
2014-10-23 23:23:40 +0800

    引入font-awesome

--- app/controllers.js
+++ app/controllers.js
@@ -6,6 +6,12 @@ angular.module('myApp.controllers', []).
     controller('LoginCtrl', [function () {
 
     }])
-    .controller('RegisterCtrl', [function () {
+    .controller('RegisterCtrl', [function ($scope) {
+
+      $scope.register = function(){
+        var data = $scope.user;
+        //....
+      };
+
 
     }]);
