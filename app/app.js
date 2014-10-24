d0ebac41f724dade3c5da80f7b1adafcb1a05361
vessnix <vessnix@gmail.com>
2014-10-24 16:40:00 +0800

    实现注册，登陆，首页

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
-angular.module('myApp', ['ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+var myApp = angular.module('myApp', ['ngRoute', 'ionic', 'myApp.filters', 'myApp.services', 'myApp.directives']).
     config(['$routeProvider', function ($routeProvider) {
-      $routeProvider.when('/', {templateUrl: 'app/partials/login.html', controller: 'LoginCtrl' });
       $routeProvider.when('/login', {templateUrl: 'app/partials/login.html', controller: 'LoginCtrl' });
       $routeProvider.when('/register', {templateUrl: 'app/partials/register.html', controller: 'RegisterCtrl' });
-      $routeProvider.otherwise({redirectTo: '/'});
+      $routeProvider.when('/home', {templateUrl: 'app/partials/home.html', controller: 'HomeCtrl' });
+      $routeProvider.otherwise({redirectTo: '/login'});
     }])
     .run(function ($rootScope) {
       $rootScope.$on('$viewContentLoaded', function () {
-        $(document).foundation();
+        AV.initialize("nt9nujwnc0vp7g1htejarpovprpsy6inrrr2ixeobaozy0xe", "k9beb8y6876zbhp5cgk4p83swuri1y2gc10f8y1tml07haam");
       });
     });
--- app/controllers.js
+++ app/controllers.js
@@ -2,16 +2,76 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-    controller('LoginCtrl', [function () {
+myApp
+    .controller('LoginCtrl', function ($scope,$ionicLoading,$location) {
+      $scope.login = function (user) {
+        $ionicLoading.show({
+          template: '正在登陆...'
+        });
+        AV.User.logIn(user.username, user.password, {
+          success: function (user) {
+            $ionicLoading.hide();
+            $location.url('/home');
+          },
+          error: function (user, error) {
+            $ionicLoading.hide();
+            debugger;
+            // The login failed. Check error to see why.
+          }
+        });
+      };
+    })
+    .controller('RegisterCtrl', function ($scope, $ionicLoading, $location) {
 
-    }])
-    .controller('RegisterCtrl', ['$scope',function ($scope) {
-      $scope.register = function(){
-        var data = $scope.user;
+      $scope.register = function () {
+        var userData = $scope.user;
 
-        //....
+        var user = new AV.User();
+        user.set("username", userData.username);
+        user.set("password", userData.password);
+
+        $ionicLoading.show({
+          template: '正在注册...'
+        });
+        user.signUp(null, {
+          success: function (user) {
+            $ionicLoading.hide();
+            $scope.login(userData);
+          },
+          error: function (user, error) {
+            debugger;
+            $ionicLoading.hide();
+            alert("Error: " + error.code + " " + error.message);
+          }
+        });
       };
 
+      $scope.login = function (user) {
+        $ionicLoading.show({
+          template: '正在登陆...'
+        });
+        AV.User.logIn(user.username, user.password, {
+          success: function (user) {
+            $ionicLoading.hide();
+            $location.url('/home');
+          },
+          error: function (user, error) {
+            $ionicLoading.hide();
+            debugger;
+            // The login failed. Check error to see why.
+          }
+        });
+      }
 
-    }]);
+
+    })
+    .controller('HomeCtrl', function ($scope, $ionicLoading, $ionicSideMenuDelegate) {
+      $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+      };
+
+    })
+    .controller('ContentCtrl', function ($scope, $ionicLoading) {
+
+
+    });
